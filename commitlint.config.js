df11f1492350c1bd8b9618d3be64952b81b4e7dd
Samer Draidi <samer.kmd@gmail.com>
2024-01-14 16:01:16 +0300

    fix: symantic-release fails if lines lenth gt than 100 char

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,9 @@ export default {
         '@commitlint/config-conventional',
     ],
     rules: {
+        // Disable the body-max-line-length rule for semantic-release
+        'body-max-line-length': [0],
+        'footer-max-line-length': [0],
         'type-enum': [
             2,
             'always',
