8ae002d2dd372c4b38d78558c17b9f2480d47564
Samer Draidi <samer.kmd@gmail.com>
2024-01-21 12:48:25 +0300

    fix: test

--- app.vue
+++ app.vue
@@ -2,9 +2,13 @@
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
+	<div class="text-center ma-8">
+		Nuxt Boilerplate Version: {{ version }}
+	</div>
 </template>
 <script setup lang="ts">
 const i18n = useI18n()
+const appConfig = useAppConfig()
 useHead(
 	{
 		htmlAttrs() {
@@ -14,9 +18,11 @@ useHead(
 		},
 		meta: [
 			{
-				name: 'description',
-				content: 'Nuxt.js project',
+				name: appConfig.app.description,
+				content: appConfig.app.description,
 			},
 		],
 	})
+
+const { version } = appConfig.app
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,6 @@
 <template>
-	<v-sheet
-		class="ma-8"
-		:border="2"
-		color="fdg"
-		:min-width="5"
-		:max-height="65"
-		:elevation="10"
-		rounded="10"
-		:height="51651651561651"
-	>
-		hid ds sd h{{ $t('hello') }}
-	</v-sheet>
 	<NuxtWelcome />
 </template>
 
 <script lang="ts" setup>
 </script>
-
-<style></style>
