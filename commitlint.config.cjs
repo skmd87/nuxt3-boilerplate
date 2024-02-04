8998fc725c3b0e576bd30cf9bfa3120983a570fc
Samer Draidi <samer.kmd@gmail.com>
2024-02-04 10:28:12 +0300

    perf: optimize eslint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
 		'type-enum': [
 			2,
 			'always',
-			['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+			['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore'],
 		],
 	},
 }
